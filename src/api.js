c919f91529236d164bf4e2a6888ff5951bc6de99
Brian Alegre <brialegre@yahoo.com>
2023-05-27 08:46:28 -0700

    Starting Search Component

---
+++ src/api.js
@@ -0,0 +1,3 @@
+var apiKey = 'b285d18d11dcc358521846452d848e59';
+var apiCall = `https://api.openweathermap.org/data/2.5/onecall`;
+var geoCall = `https://api.openweathermap.org/geo/1.0/direct`;
---
+++ src/components/search/search.js
@@ -0,0 +1,11 @@
+
+
+
+// Search Component
+const Search = () => {
+
+}
+
+
+// Export Component
+export default Search 
