a721c35d1be27595d9a6a26f56550cb79b9ff1d3
Brian Alegre <brialegre@yahoo.com>
2023-05-28 22:50:29 -0700

    Added handling of search terms

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 // Import
 import Search from "./components/search/search"
 import './App.css';
+import { useState } from "react"
 
 function App() {
+
+  const hanldeUserInput = (searchText) => {
+    console.log(searchText)
+    const [lat, lon] = searchText.value.split(" ")
+  }
+
   return (
-    <Search></Search>
+    <Search userInput={hanldeUserInput} />
   )
 }
 
