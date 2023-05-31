a0f673ae905368fb45e9c892c2a27d434fb6c6f6
Brian Alegre <brialegre@yahoo.com>
2023-05-30 21:10:34 -0700

    Testing API calls

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import Search from "./components/search/search"
 import './App.css';
 // import { useState } from "react"
+import { APIKey, WeatherGeoAPI } from "./api"
 
 function App() {
 
@@ -9,6 +10,24 @@ function App() {
   //   console.log(searchText)
   //   const [lat, lon] = searchText.value.split(" ")
   // }
+
+  // City Name = Tokyo
+  function getLatLon(cityName) {
+
+    // Geo Call, Fetch
+    fetch(WeatherGeoAPI + `q=${encodeURI(cityName)}&appid=${APIKey}`)
+    console.log("getLatLon Called")
+      .then(function (response) {
+        if (response.okay) {
+          response.json()
+            .then(function (data) {
+              console.log(data)
+            })
+        }
+      })
+  }
+
+  getLatLon("tokyo")
 
   return (
     <Search />
