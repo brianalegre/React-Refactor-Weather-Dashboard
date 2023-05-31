f3d93c14807a2bf21782d026ba10f0d4965397a1
Brian Alegre <brialegre@yahoo.com>
2023-05-30 21:33:39 -0700

    Added useState to save Lat and Lon from API call.  Need to debug saving state for Lon

--- src/App.js
+++ src/App.js
@@ -1,42 +1,32 @@
 // Import
 import Search from "./components/search/search"
 import './App.css';
-// import { useState } from "react"
+import { useState } from "react"
 import { APIKey, WeatherGeoAPI, WeatherDataAPI } from "./api"
 
 function App() {
 
-  // const hanldeUserInput = (searchText) => {
-  //   console.log(searchText)
-  //   const [lat, lon] = searchText.value.split(" ")
-  // }
+  // Set States for Latitude and Longtidue
+  const [lat, setLat] = useState("");
+  const [lon, setLon] = useState("");
 
-  // City Name = Tokyo
-  // function getLatLon(cityName) {
 
-  //   // Geo Call, Fetch
-  //   fetch(WeatherGeoAPI + `q=${encodeURI(cityName)}&appid=${APIKey}`)
-  //   console.log("getLatLon Called")
-  //     .then(function (response) {
-  //       if (response.okay) {
-  //         response.json()
-  //           .then(function (data) {
-  //             console.log(data)
-  //           })
-  //       }
-  //     })
-  // }
 
+  // Get Latitude and Longtidue from WeatherGEOAPI
   const getLatLon = async (citySearch) => {
     const url = `${WeatherGeoAPI}q=${citySearch}&apikey=${APIKey}`;
 
     return fetch(url)
-      .then((response) => response.json())
+      .then((response) => response.json()
+      )
       .catch((error) => console.log(error))
   }
-
   getLatLon('tokyo')
-    .then((data) => console.log(data));
+    .then((data) =>
+      // console.log(data, 'lat:', data[0].lat, 'lon:', data[0].lon)),
+      setLat(data[0].lat), console.log("Setting Lat to:", lat),
+      // setLon(data[0].lon), console.log("Setting Lon to:", lon)
+    );
 
 
   return (
