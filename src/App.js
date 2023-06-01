9ea3a032cc2a2e702618b5e944dacd3b3bd8b1a3
Brian Alegre <brialegre@yahoo.com>
2023-05-31 21:39:43 -0700

    Testing CurrentWeather API

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import { APIKey, WeatherGeoAPI, WeatherDataAPI } from "./api"
 function App() {
 
   // Set States for Latitude and Longtidue
-  const [lat, setLat] = useState("");
-  const [lon, setLon] = useState("");
+  const [lat, setLat] = useState("35.6828387");
+  const [lon, setLon] = useState("139.7594549");
 
 
 
-  // Get Latitude and Longtidue from WeatherGEOAPI
+  // Get Latitude and Longtidue from WeatherGeoAPI
   const getLatLon = async (citySearch) => {
     const url = `${WeatherGeoAPI}q=${citySearch}&apikey=${APIKey}`;
 
@@ -23,11 +23,27 @@ function App() {
   }
   getLatLon('tokyo')
     .then((data) =>
+      console.log("I am getLatLong", data)
       // console.log(data, 'lat:', data[0].lat, 'lon:', data[0].lon)),
-      setLat(data[0].lat), console.log("Setting Lat to:", lat),
+      // setLat(data[0].lat), console.log("Setting Lat to:", lat),
       // setLon(data[0].lon), console.log("Setting Lon to:", lon)
     );
 
+  // Get Current Weather from WeatherDataAPI
+  const getCurrentWeather = async () => {
+    const url2 = `${WeatherDataAPI}${lat}&lon${lon}&appid=${APIKey}`;
+
+    return fetch(url2)
+      .then((response) => response.json()
+      )
+      .catch((error) => console.log(error))
+  }
+
+  getCurrentWeather()
+    .then((data) =>
+      console.log("I am get currentWeather", data),
+      console.log(lat, lon)
+    )
 
   return (
     <Search />
--- src/api.js
+++ src/api.js
@@ -1,4 +1,6 @@
 // Export Component
 export const APIKey = 'b285d18d11dcc358521846452d848e59';
-export const WeatherDataAPI = `https://api.openweathermap.org/data/2.5/onecall`;
-export const WeatherGeoAPI = `https://api.openweathermap.org/geo/1.0/direct?`;
+// export const WeatherDataAPI = `https://api.openweathermap.org/data/2.5/onecall`;
+export const WeatherDataAPI = 'https://api.openweathermap.org/data/2.5/weather?lat='
+// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+export const WeatherGeoAPI = 'https://api.openweathermap.org/geo/1.0/direct?';
