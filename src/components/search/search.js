647b3f579bf13df9f4644404ed9a6b2765b385b3
Brian Alegre <brialegre@yahoo.com>
2023-06-06 21:39:09 -0700

    testing tailwind

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -19,13 +19,25 @@ const Search = () => {
 
 
     return (
-        <div className="searchBar">
-            <input
-                type="text"
-                onChange={handleUserInput}
-                placeholder="Enter a City Name"
-                value={search}
-            />
+        // <div className="searchBar">
+        //     <input
+        //         type="text"
+        //         onChange={handleUserInput}
+        //         placeholder="Enter a City Name"
+        //         value={search}
+        //     />
+        // </div>
+        <div className="flex items-center">
+            <div className="flex border border-purple-200 rounded">
+                <input
+                    type="text"
+                    className="block w-full px-4 py-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                    placeholder="Search..."
+                />
+                <button className="px-4 text-white bg-purple-600 border-l rounded ">
+                    Search
+                </button>
+            </div>
         </div>
     )
 
