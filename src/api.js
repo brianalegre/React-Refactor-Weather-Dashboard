730a798a7d35d69d0eaa642e6b5240f56bb4cb82
Brian Alegre <brialegre@yahoo.com>
2023-06-12 22:05:37 -0700

    Updated Search Component, updated API calls

--- src/api.js
+++ src/api.js
@@ -3,4 +3,12 @@ export const APIKey = 'b285d18d11dcc358521846452d848e59';
 // export const WeatherDataAPI = `https://api.openweathermap.org/data/2.5/onecall`;
 export const WeatherDataAPI = 'https://api.openweathermap.org/data/2.5/weather?lat='
 // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-export const WeatherGeoAPI = 'https://api.openweathermap.org/geo/1.0/direct?';
+export const WeatherGeoAPI = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Key": "445fda8cf1msh70da80e5f525233p18eca4jsn9bcc56577356",
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+};
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,60 +1,106 @@
 // Import
 import React, { useState } from "react";
 import "./search.css";
+import { APIKey, WeatherGeoAPI, WeatherDataAPI, geoApiOptions } from "../../api"
+import { AsyncPaginate } from "react-select-async-paginate";
 
 
 // Search Component
 const Search = ({ onSearchChange }) => {
 
-    // Create State from Search
-    const [search, setSearch] = useState("");
 
-    // Update State
-    const handleUserInput = (event) => {
-        setSearch(event.target.value)
-        console.log("this is handleUserInput", search)
-    };
+    // Search State
+    const [search, setSearch] = useState(null)
 
-    console.log('hi')
+    // Retrieve input values
+    const loadOptions = (inputValue) => {
+        return fetch(
+            `${WeatherGeoAPI}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+            geoApiOptions
+        )
+            .then((response) => response.json())
+            .then((response) => {
+                return {
+                    options: response.data.map((city) => {
+                        console.log(response.data)
+                        return {
+                            value: `${city.latitude} ${city.longitude}`,
+                            label: `${city.name}, ${city.countryCode} `,
+                        };
+                    }),
+                }
+            })
+        // .catch(err => console.log(err))
+    }
 
+
+    // Handle On Change
+    const handleOnChange = (searchData) => {
+        setSearch(searchData);
+        onSearchChange(searchData);
+    }
 
     return (
-        // <div className="searchBar">
-        //     <input
-        //         type="text"
-        //         onChange={handleUserInput}
-        //         placeholder="Enter a City Name"
-        //         value={search}
-        //     />
-        // </div>
-        <div className="flex items-center">
-            <div className="flex space-x-1">
-                <input
-                    type="text"
-                    className="block w-full px-4 py-2 text-purple-700 bg-white border rounded-full focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                    placeholder="Search..."
-                />
-                <button className="px-4 text-white bg-purple-600 rounded-full ">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-5 h-5"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                        />
-                    </svg>
-                </button>
-            </div>
-        </div>
+        <AsyncPaginate
+            placeholder="Search for city..."
+            // Milliseconds to wait before sending a request
+            debounceTimeout={600}
+            value={search}
+            onChange={handleOnChange}
+            loadOptions={loadOptions}
+        />
     )
-
 }
+
+// // Create State from Search
+// const [search, setSearch] = useState("");
+
+// // Update State
+// const handleUserInput = (event) => {
+//     setSearch(event.target.value)
+//     console.log("this is handleUserInput", search)
+// };
+
+// console.log('hi')
+
+
+// return (
+//     // <div className="searchBar">
+//     //     <input
+//     //         type="text"
+//     //         onChange={handleUserInput}
+//     //         placeholder="Enter a City Name"
+//     //         value={search}
+//     //     />
+//     // </div>
+//     <div className="flex items-center">
+//         <div className="flex space-x-1">
+//             <input
+//                 type="text"
+//                 className="block w-full px-4 py-2 text-purple-700 bg-white border rounded-full focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+//                 placeholder="Search..."
+//             />
+//             <button className="px-4 text-white bg-purple-600 rounded-full ">
+//                 <svg
+//                     xmlns="http://www.w3.org/2000/svg"
+//                     className="w-5 h-5"
+//                     fill="none"
+//                     viewBox="0 0 24 24"
+//                     stroke="currentColor"
+//                     strokeWidth={2}
+//                 >
+//                     <path
+//                         strokeLinecap="round"
+//                         strokeLinejoin="round"
+//                         d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+//                     />
+//                 </svg>
+//             </button>
+//         </div>
+//     </div>
+// )
+
+// }
 
 
 // Export Component
